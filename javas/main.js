2d556a58b05db72dcc193b8806aff0d49fda4a8c
fuckcorona <hikmet.xidirzada57@mail.ru>
2021-08-31 12:39:19 +0400

    exam

---
+++ javas/main.js
@@ -0,0 +1,18 @@
+var btn=document.querySelectorAll(".box1 button")
+var text1=document.querySelector(".txt1")
+var text2=document.querySelector(".txt2")
+var text3=document.querySelector(".txt3")
+
+var btn=onclick=function(){
+ for (let i = 0; i < btn.length; i++) {
+     for (let d = 0; d < txt1.length; d++) {
+         
+        this.nextElementSibling.add("active")
+     }
+   
+    
+
+     
+ }
+
+}
