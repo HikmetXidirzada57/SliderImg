86c82d06e3785e9189ff58acf879f4173d21694b
hikmet <hikmet_xidirzada57@mail.ru>
2022-08-17 21:01:10 +0400

    imgSlider

--- javas/main.js
+++ javas/main.js
@@ -1,18 +1,26 @@
-var btn=document.querySelectorAll(".box1 button")
-var text1=document.querySelector(".txt1")
-var text2=document.querySelector(".txt2")
-var text3=document.querySelector(".txt3")
+// var btn=document.querySelectorAll(".box1 button")
+// var text1=document.querySelector(".txt1")
+// var text2=document.querySelector(".txt2")
+// var text3=document.querySelector(".txt3")
 
-var btn=onclick=function(){
- for (let i = 0; i < btn.length; i++) {
-     for (let d = 0; d < txt1.length; d++) {
+// btn.onclick=function(){
+//  for (let i = 0; i < btn.length; i++) {
+//      for (let d = 0; d < txt1.length; d++) {
          
-        this.nextElementSibling.add("active")
-     }
+//         this.nextElementSibling.add("active")
+//      }
    
     
 
      
- }
+//  }
 
+// }
+const leftimgs=document.querySelectorAll(".carouselItem img")
+const rightImg=document.querySelector(".carousel-img img")
+for( let img of leftimgs){
+    img.onclick=function(){
+       let imgSrc=this.src
+       rightImg.src=img.src
+    }
 }
